4a96eeda43df21b305058f8ec6c59d05768d1a89
pr-juan-k <djjuankalexis18@gmail.com>
2025-03-30 23:33:51 -0300

    ejercicio 2.4 completado

--- tp1_1.c
+++ tp1_1.c
@@ -6,9 +6,9 @@ int main(){
     int *punt;
     punt = num;
     printf("\nEl contenido del puntero:%d",*punt);
-    printf("\nLa direccion de memoria que guarda el puntero:%p",punt);
-    printf("\nLa direccion de memoria de la variable:%d",&num);
-    printf("\nLa direccion de memoria del puntero:%d",&punt);
-    printf("\nTamanio de memoria de la variable:%d",sizeof(num));
+    printf("\n La direccion de memoria que guarda el puntero:%p",punt);
+    printf("\n La direccion de memoria de la variable:%d",&num);
+    printf("\n La direccion de memoria del puntero:%d",&punt);
+    printf("\n Tamanio de memoria de la variable:%d",sizeof(num));
     return 0;
 }
